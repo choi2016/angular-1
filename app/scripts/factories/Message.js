b2e96e0623befe89d9f5c0fbdb1365c0ea148bc4
Edward Choi <edwardchoi.2011@gmail.com>
2017-09-04 13:48:00 -0700

    made message info appear

--- app/scripts/factories/Message.js
+++ app/scripts/factories/Message.js
@@ -3,6 +3,7 @@
 		var Message = {};
 		var ref = firebase.database().ref().child("messages");
 		var messages = $firebaseArray(ref);
+		console.log(messages);
 
 		Message.getByRoomID = function(roomID) {
 			return $firebaseArray(ref.orderByChild('roomID').equalTo(roomID));
